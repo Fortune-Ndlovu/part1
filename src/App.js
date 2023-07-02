acc6bfb060bbca9ff797237fcddb3c4f68b737a5
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-02 16:30:21 +0100

    Creating helper component that calculates the year of birth based on age

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-const Hello =  (props) => {
+const Hello = (props) => {
+  console.log(props);
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+      <p>So you are probably born in {bornYear()}</p>
     </div>
   )
 }
 
 function App() {
+  const name = "John";
+  const age = 22;
+
   return (
     <div>
        <p>Greetings</p>
-       <Hello name="Fortune"/>
-       <Hello name="Marry"/>
+       <Hello name={name} age={2 * 10 + 1}/>
+       <Hello name="Mary"age={age}/>
     </div>
   );
 }
